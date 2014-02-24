7f949ca6305159534eb5552fedd12550734a5fe6
tasinclair1 <tasinclair1@gmail.com>
2014-02-24 23:14:08 +0000

    Added methods and variables to Player class

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -2,6 +2,32 @@ package Game;
 
 public class Player {
 	
+	private String name;
+	private int score;
 	
+	public Player(String n) {
+		
+		name = n;
+		score = 0;
+		
+	}
+	
+	public String getName() {
+		
+		return name;
+		
+	}
+	
+	public int getScore() {
+		
+		return score;
+		
+	}
+	
+	public void updateScore(int points) {
+		
+		score = score + points;
+		
+	}	
 
 }
