d31f5f2d95f1f8741c2288487bf0aac0cb7a4e6f
Thomas Alexander Sinclair CSM2012 <mlb12174@muir-03.cis.strath.ac.uk>
2014-03-10 11:56:35 +0000

    Added timer to conundrum

--- src/Game/Conundrum.java
+++ src/Game/Conundrum.java
@@ -6,9 +6,14 @@ public class Conundrum extends Round {
 	
 	private String answer1;
 	private String answer2;
+	boolean player;
+	
+	Timer timer;
 	
 	public Conundrum(Dictionary dict, Player pOne, Player pTwo, Input i) {
 		super(dict, pOne, pTwo, i);
+		
+		timer = new Timer();
 	}
 	
 	public void play() {
@@ -20,48 +25,81 @@ public class Conundrum extends Round {
 		String answer = temp[0];
 		String anagram = temp[1];
 		
+		String keyPress = "";
+		
 		System.out.println("The conundrum is: " + anagram + " " + answer); //answer just for testing
 		
-		for(int i = 5; i > 0; i--) {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println(i);
+		timer.schedule(new TheTimer(), 5000);
+		
+		while (keyPress.equalsIgnoreCase("q") == false && keyPress.equalsIgnoreCase("p") == false) {
+			keyPress = i.getWord();
+		       
+		       if(keyPress.equals("q")) {
+					
+		    	    timer.cancel();
+					System.out.println("Player 1 enter answer: ");
+					answer1 = i.getWord();
+					System.out.println("Your answer is: " + answer1);
+					player = true;
+					
+				} else if(keyPress.equals("p")) {
+					
+					timer.cancel();
+					System.out.println("Player 2 enter answer: ");
+					answer1 = i.getWord();
+					System.out.println("Your answer is: " + answer1);
+					player = false;
+					
+				} else {
+					
+					System.out.println("Incorrect Input");
+					
+				}
 		}
 		
-		System.out.println("Player 1 enter answer: ");
-		answer1 = i.getWord();
-		System.out.println("Your answer is: " + answer1);
-		
-		System.out.println("Player 2 enter answer: ");
-		answer2 = i.getWord();
-		System.out.println("Your answer is: " + answer2);
-		
-		if(answer1.equals(answer) && !answer2.equals(answer)) {
+		/*
+		 * if the first answer is correct then award points to the player whom entered it
+		 * else get answer from the other player
+		 * 		if that is correct award points
+		 * 		else no points for anyone
+		 */
+		if(answer1.equals(answer)) {
 			
-			System.out.println("Player 1 wins");
-			pOne.updateScore(10);
-			
-		} else if(!answer1.equals(answer) && answer2.equals(answer)) {
-			
-			System.out.println("Player 2 wins");
-			pTwo.updateScore(10);
-			
-		} else if(answer1.equals(answer) && answer2.equals(answer)) {
-			
-			System.out.println("Both correct");
-			pOne.updateScore(5);
-			pTwo.updateScore(5);
+			awardPoints(player);
 			
 		} else {
 			
-			System.out.println("Both wrong. The answer was: " + answer);
+			if (player){
+				System.out.println("Incorrect, player 2 enter answer: ");
+				answer2 = i.getWord();
+				System.out.println("Your answer is: " + answer2);
+			}else{
+				System.out.println("Incorrect, player 1 enter answer: ");
+				answer2 = i.getWord();
+				System.out.println("Your answer is: " + answer2);
+			}
+			
+			if (answer2.equals(answer)){
+				
+				awardPoints(!player);
+				
+			}else{
+				System.out.println("Both wrong. The answer was " + answer);
+			}
 			
 		}
 		
+	}
+	
+	private void awardPoints(boolean player){
+		
+		if (!player){
+			System.out.println("Player 1 wins");
+			pOne.updateScore(10);
+		}else{
+			System.out.println("Player 2 wins");
+			pTwo.updateScore(10);
+		}
 	}
 
 }
