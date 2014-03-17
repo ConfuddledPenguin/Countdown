fec82eae33f64f21521b1af8a7b4734550c34423
tasinclair1 <tasinclair1@gmail.com>
2014-03-17 10:54:43 +0000

    commiting my mess to acces it at uni

--- src/Game/Word.java
+++ src/Game/Word.java
@@ -1,28 +1,94 @@
 package Game;
 
-public class Word {
-	
-	private Player playerOne;
-	private Player playerTwo;
+import java.util.ArrayList;
+import java.util.Timer;
 
-	private String answer1;
-	private String answer2;
+public class Word extends Round{
+
+	//stores the answer entered by the player
+	private String playerAnswer;
+	//Used to track which player has 'buzzed'
+	//true for player one and false for player 2
+	boolean player;
+
+	Timer timer;
+
+	public Word(Dictionary dict, Player pOne, Player pTwo, Input i) {
+		super(dict, pOne, pTwo, i);
+
+		timer = new Timer();
+	}
 
 	public void play() {
 
-		System.out.println("Word");
+		System.out.println("Word\n");
+		
+		String letters = getLetters();
 
+		ArrayList<String> answers = dict.getBestWords(letters);
+
+		String keyPress = "";
+
+		//currently printing the answer for testing purposes
+		System.out.println("The letters are: " + letters+ "\n");
+
+		timer.schedule(new TheTimer(), 10000);
+
+		while (/* Timer */) {
+
+			
+
+		}
+		
+		player = true;
+		collectAnswer(player);
+		collectAnswer(!player);
+		
+		if(rankAnswer(player) < rankAnswer(!player)) {
+			awardPoints(player);
+		} else {	
+			awardPoints(!player);	
+		}
+	}
+	
+	/*return position in array of players word. Smaller is better*/
+	private int rankAnswer(boolean player) {
+		
+		return 0;
+		
+	}
+	
+	/*get letters*/
+	private String getLetters() {
+		
+		return null;
+		
 	}
 
-	public String returnAnswer1() {
+	private void awardPoints(boolean player) {
 
-		return answer1;
-
+		if (player){
+			System.out.println("Player 1 wins");
+			pOne.updateScore(10);
+		} else {
+			System.out.println("Player 2 wins");
+			pTwo.updateScore(10);
+		}
 	}
 
-	public String returnAnswer2() {
+	private void collectAnswer(Boolean whatPlayer) {
 
-		return answer2;
+		int playerNo;
+
+		if(whatPlayer) {
+			playerNo = 1;
+		} else {
+			playerNo = 2;
+		}
+
+		System.out.println("Player " + playerNo + " enter answer: ");
+		playerAnswer = i.getWord();
+		System.out.println("Your answer is: " + playerAnswer + "\n");
 
 	}
 
